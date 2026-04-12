a6890e2b65ee6b62a77eacd0d3cb0e0241c6a287
Diego Ramos dos Santos <diegorsantos09@outlook.com>
2026-04-12 20:36:32 -0300

    Initial Commit - Setting Spring Project

---
+++ src/test/java/com/diegoramos/mylifediary/MylifediaryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.diegoramos.mylifediary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MylifediaryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
