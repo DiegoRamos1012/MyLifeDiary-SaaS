e591560334155b38289b163f919523b9456d5512
Diego Ramos dos Santos <diegorsantos09@outlook.com>
2026-04-16 23:27:43 -0300

    Removing tag public in Main to avoid redundancy with Java 25

--- src/main/java/com/diegoramos/mylifediary/MylifediaryApplication.java
+++ src/main/java/com/diegoramos/mylifediary/MylifediaryApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class MylifediaryApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(MylifediaryApplication.class, args);
-	}
+    static void main(String[] args) {
+        SpringApplication.run(MylifediaryApplication.class, args);
+    }
 
 }
